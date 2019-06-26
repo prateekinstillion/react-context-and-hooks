6c0cf10c024e04be99068f04c1b799573348e1ea
prateekinstillion <Prateek Madaan>
2019-06-26 10:44:40 +0530

    better way to consume context this is what is best to use

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -3,21 +3,25 @@ import { ThemeContext } from "../contexts/ThemeContext";
 
 class AppNavbar extends Component {
   // Set the context type and the context which we want to consume
-  static contextType = ThemeContext;
 
   render() {
-    console.log(this.context); // Access to the shared data on this.context
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context Application</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context Application</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
