7e90ee249980c2ef78b948ed4bc2a171b3bd1ba1
prateekinstillion <Prateek Madaan>
2019-06-26 10:24:42 +0530

    creating context and the context provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import AppNavbar from "./components/AppNavbar";
 import BookList from "./components/BookList";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <AppNavbar />
-      <BookList />
+      <ThemeContextProvider>
+        <AppNavbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
