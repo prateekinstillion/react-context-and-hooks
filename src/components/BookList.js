82557b9d500f6cc3d9525d444f6b2afab922881f
prateekinstillion <Prateek Madaan>
2019-06-26 10:39:16 +0530

    exercise of consuming the context in the booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class BookList extends Component {
+  // Consumer of the ThemeContext
+  static contextType = ThemeContext;
   render() {
+    // console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ backgroundColor: theme.bg, color: theme.syntax }}
+      >
         <ul>
-          <li>the way of kings</li>
-          <li>the name of the wind</li>
-          <li>the final empire</li>
+          <li style={{ background: theme.ui }}>the way of kings</li>
+          <li style={{ background: theme.ui }}>the name of the wind</li>
+          <li style={{ background: theme.ui }}>the final empire</li>
         </ul>
       </div>
     );
