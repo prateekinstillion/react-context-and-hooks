f174558e771ec8603d83f646019442bd9dfee71a
prateekinstillion <Prateek Madaan>
2019-06-26 10:32:53 +0530

    added the light theme on navbar

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class AppNavbar extends Component {
+  // Set the context type and the context which we want to consume
+  static contextType = ThemeContext;
+
   render() {
+    console.log(this.context); // Access to the shared data on this.context
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Context Application</h1>
         <ul>
           <li>Home</li>
